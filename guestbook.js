768e9d367268b6e89d0f6088b9d46dbbae297680
naveenbharadwaj-nc <102787031+naveenbharadwaj-nc@users.noreply.github.com>
2022-04-01 12:09:52 +0530

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://24b287c6.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
